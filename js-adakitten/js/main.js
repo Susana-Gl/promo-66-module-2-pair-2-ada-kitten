233d8aa7b88fad6a42ee197574b20b4aa728d71c
Emma Esser Díaz <emma@MacBook-Pro-de-Emma-2.local>
2026-03-10 21:42:32 +0100

    pair primer dia

---
+++ js-adakitten/js/main.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const kittenList = document.querySelector (".js-list");
+const btnMas = document.querySelector(".js-btn-add");
+const newForm = document.querySelector(".js-new-form");
+
+console.log(newForm);
+
+btnMas.addEventListener("click", () => {
+  newForm.classList.toggle("collapsed");
+})
+
+
+const kittenOne = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src="https://api-pw.dev.adalab.es/gato-siames.webp"
+    alt="gatito"
+  />
+  <h3 class="card_title">Anastacio</h3>
+  <h4 class="card_race">Siamés</h4>
+  <p class="card_description">
+            Porte elegante, su patrón de color tan característico y sus ojos
+            de un azul intenso, pero su historia se remonta a Asía al menos
+            hace 500 años, donde tuvo su origen muy posiblemente.
+   </p>
+</article>
+</li>`;
+
+const kittenTwo = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src="https://api-pw.dev.adalab.es/sphynx-gato.webp"
+    alt="gatito"
+  />
+  <h3 class="card_title">Fiona</h3>
+  <h4 class="card_race">Sphynx</h4>
+  <p class="card_description">
+            Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.
+   </p>
+</article>
+</li>`;
+
+const kittenThree = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src="https://api-pw.dev.adalab.es/maine-coon-cat.webp"
+    alt="gatito"
+  />
+  <h3 class="card_title">Cielo</h3>
+  <h4 class="card_race">Maine Coon</h4>
+  <p class="card_description">
+          Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.
+   </p>
+</article>
+</li>`;
+
+kittenList.innerHTML = kittenOne+kittenTwo+kittenThree;
