1dbba577aa185c69e6e4b5d33215ba111c655534
Emma Esser Díaz <emma@MacBook-Pro-de-Emma-2.local>
2026-03-14 13:32:33 +0100

    sabado 14 de marzo

--- js-adakitten/js/main.js
+++ js-adakitten/js/main.js
@@ -7,23 +7,25 @@ const btnMas = document.querySelector(".js-btn-add");
 const newForm = document.querySelector(".js-new-form");
 const searchDesc = document.querySelector (".js_in_search_desc")
 const searchBtn = document.querySelector (".js_button-search")
+const breed = document.querySelector (".js_search_breed")
 
 
 //DATOS
 
 const kittenDesc1 = "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente."
+const kittenBreed1 = "Siamés"
 
 const kittenDesc2 = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo."
+const kittenBreed2 = "Sphynx"
 
 const kittenDesc3 = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta."
+const kittenBreed3 = "Maine Coon"
+
+let breedText ="";
+
 
 //FUNCIONES
 
-/*if (kittenDesc1.includes(descrSearchText)) {
-  kittenList.style.display = "block";
-} else {
-  kittenList.style.display = "none";
-}*/
 //FUNCIONES EVENTOS
 
 //EVENTOS
@@ -42,7 +44,37 @@ searchBtn.addEventListener("click", (ev) => {
     kittenList.innerHTML += kittenTwo;}
   if (kittenDesc3.includes(descrSearchText)) {
     kittenList.innerHTML += kittenThree;}
+
+    const searchBreed = breed.value
+  kittenList.innerHTML =""
+  if (kittenBreed1.includes(searchBreed)) {
+    kittenList.innerHTML += kittenOne;}
+  if (kittenBreed2.includes(searchBreed)) {
+    kittenList.innerHTML += kittenTwo;}
+  if (kittenBreed3.includes(searchBreed)) {
+    kittenList.innerHTML += kittenThree;}
+
+  if (kittenBreed1 === "") {
+    breedText = `Uy que despiste, no sabemos su raza`;
+  } else {
+    breedText = kittenBreed1
+  }
+
+  if (kittenBreed2 === "") {
+    breedText = `Uy que despiste, no sabemos su raza`;
+  } else {
+    breedText = kittenBreed2
+  }
+
+  if (kittenBreed3 === "") {
+    card_race.innerHTML = `Uy que despiste, no sabemos su raza`
+    /* breedText = `Uy que despiste, no sabemos su raza`; */
+  } else {
+    breedText = kittenBreed3
+  }
 })
+
+
 
 //ACCIONES AL CARGAR LA PAGINA
 
